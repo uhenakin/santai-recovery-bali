9b02110588dd307da92c0db543d95404dd17ad19
uhenakin <uhenakin@gmail.com>
2026-06-04 10:14:52 +0800

    fix: suppress ReactLenis type error for React 19

--- components/SmoothScrolling.tsx
+++ components/SmoothScrolling.tsx
@@ -1,10 +1,10 @@
 "use client";
-
 // @ts-ignore
 import { ReactLenis } from "@studio-freight/react-lenis";
 
 export default function SmoothScrolling({ children }: { children: React.ReactNode }) {
   return (
+    // @ts-ignore
     <ReactLenis root options={{ lerp: 0.05, duration: 1.5, smoothWheel: true, wheelMultiplier: 1.2 }}>
       {children}
     </ReactLenis>
